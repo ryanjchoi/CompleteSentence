6d8d3bd83d1861cc212a7292720e0001f33da1fb
Ryan Choi <ryan.choi@cbc.ca>
2018-04-02 08:17:04 -0400

    add quotes data structure

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@
 * @Author: Ryan Choi
 * @Date:   2018-03-31 09:50:48
 * @Last Modified by:   Ryan Choi
-* @Last Modified time: 2018-04-02 07:53:06
+* @Last Modified time: 2018-04-02 08:16:01
 */
 
 import React from 'react';
 import Game from './Game';
 
 class App extends React.Component {
-    // sentence = "A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.";
-    sentence = "A pessimist sees the difficulty in every opportunity;";
-    author = "Winston Churchill";
-    seconds = this.sentence.split(" ").length * 2;
+    quotes = [{
+        longSentence: "A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.",
+        sentence: "An optimist sees the opportunity in every difficulty.",
+        author: "Winston Churchill",
+    }];
+
+    quote = this.quotes[0];
+
+    seconds = this.quote.sentence.split(" ").length * 2;
 
     state = {
         gameId: 1,
@@ -28,8 +33,8 @@ class App extends React.Component {
         return (
             <Game
                 key={this.state.gameId}
-                sentence={this.sentence}
-                author={this.author}
+                sentence={this.quote.sentence}
+                author={this.quote.author}
                 initialSeconds={this.seconds}
                 onPlayAgain={this.resetGame}
             />
