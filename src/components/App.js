2c662f1ea2b9fa5051e82c6e610004cae83ceb62
Ryan Choi <ryan.choi@cbc.ca>
2018-04-08 17:22:42 -0400

    fix the getSeconds bug

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 * @Author: Ryan Choi
 * @Date:   2018-03-31 09:50:48
 * @Last Modified by:   Ryan Choi
-* @Last Modified time: 2018-04-08 12:44:59
+* @Last Modified time: 2018-04-08 17:20:59
 */
 
 // hide a warning message in the bottom of the simulator
@@ -17,26 +17,34 @@ export default class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.belt = this.getBelt();
+        this.quote = this.getQuote();
+        this.seconds = this.getSeconds(this.quote.sentence);
+
+        this.state = {
+            gameId: 1,
+            quote: this.quote,
+            seconds: this.seconds,
+        };
     };
 
-    quote = SENTENCES.QUOTES[Math.floor(Math.random() * SENTENCES.QUOTES.length)];
-
-    currentBelt = CONSTANTS.BELT.YELLOW;
-
-    seconds = Math.ceil(this.quote.sentence.split(" ").length * this.currentBelt);
+    getBelt() {
+        return CONSTANTS.BELT.YELLOW;
+    };
 
     getQuote = () => {
-        return SENTENCES.QUOTES[Math.floor(Math.random() * SENTENCES.QUOTES.length)];
-    }
+        let quote = SENTENCES.QUOTES[Math.floor(Math.random() * SENTENCES.QUOTES.length)];
+        quote.seconds = this.getSeconds(quote.sentence)
 
-    state = {
-        gameId: 1,
-        quote: this.getQuote(),
+        return quote;
+    };
+
+    getSeconds(sentence: String) {
+        return Math.ceil(sentence.split(" ").length * this.belt);
     };
 
     resetGame = () => {
-        let aQuote = this.getQuote();
-
         this.setState((prevState) => {
             return { gameId: prevState.gameId + 1};
         });
@@ -57,7 +65,7 @@ export default class App extends React.Component {
                 key={this.state.gameId}
                 sentence={this.state.quote.sentence}
                 author={this.state.quote.author}
-                initialSeconds={this.seconds}
+                initialSeconds={this.state.quote.seconds}
                 onPlayAgain={this.resetGame}
                 onNewQuote={this.loadNewQuote}
             />
--- src/components/sentences.js
+++ src/components/sentences.js
@@ -2,15 +2,14 @@
 * @Author: Ryan Choi
 * @Date:   2018-04-07 06:46:15
 * @Last Modified by:   Ryan Choi
-* @Last Modified time: 2018-04-07 07:29:32
+* @Last Modified time: 2018-04-08 12:58:49
 */
 
 export const QUOTES = [
-    {
-        // sentence: "A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.",
-        sentence: "An optimist sees the opportunity in every difficulty.",
-        author: "Winston Churchill",
-    },
+    // {
+    //     sentence: "A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.",
+    //     author: "Winston Churchill",
+    // },
     // {
     //     sentence: "Treat a man as he is and he will remain as he is. Treat a man as he can and should be and he will become as he can and should be.",
     //     author: "Stephen R. Covey, The 7 Habits of Highly Effective People: Powerful Lessons in Personal Change"
@@ -23,22 +22,22 @@ export const QUOTES = [
         sentence: "All our knowledge has its origin in our perceptions",
         author: "Leonardo da Vinci"
     },
-    {
-        sentence: "For the best return on your money, pour your purse into your head.",
-        author: "Benjamin Franklin"
-    },
-    {
-        sentence: "Absorb what is useful, Discard what is not, Add what is uniquely your own.",
-        author: "Bruce Lee"
-    },
-    {
-        sentence: "Life isn’t about finding yourself. Life is about creating yourself.",
-        author: "George Bernard Shaw"
-    },
-    {
-        sentence: "We are what we repeatedly do. Excellence, then, is not an act but a habit.",
-        author: "Will Durant"
-    },
+    // {
+    //     sentence: "For the best return on your money, pour your purse into your head.",
+    //     author: "Benjamin Franklin"
+    // },
+    // {
+    //     sentence: "Absorb what is useful, Discard what is not, Add what is uniquely your own.",
+    //     author: "Bruce Lee"
+    // },
+    // {
+    //     sentence: "Life isn’t about finding yourself. Life is about creating yourself.",
+    //     author: "George Bernard Shaw"
+    // },
+    // {
+    //     sentence: "We are what we repeatedly do. Excellence, then, is not an act but a habit.",
+    //     author: "Will Durant"
+    // },
     {
         sentence: "What we think, we become.",
         author: "Buddha"
