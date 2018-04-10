9696dd13d861385511cf717eb79a0be4cf7dd18d
Ryan Choi <ryan.choi@cbc.ca>
2018-04-10 13:38:20 -0400

    change vars to consts

--- src/components/db.js
+++ src/components/db.js
@@ -2,27 +2,27 @@
 * @Author: Ryan Choi
 * @Date:   2018-04-09 17:26:18
 * @Last Modified by:   Ryan Choi
-* @Last Modified time: 2018-04-10 13:30:03
+* @Last Modified time: 2018-04-10 13:37:16
 */
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017/CompleteSentence';
+const url = 'mongodb://localhost:27017/CompleteSentence';
 
-var findDocuments = function(database, callback) {
+const findDocuments = function(database, callback) {
     const db = database.db("CompleteSentence");
-    var collection = db.collection('quotes');
+    const collection = db.collection('quotes');
 
     collection.find({"author":"Winston Churchill"}).toArray(function(err, docs) {
         console.log(docs);
         callback;
-    })
-}
+    });
+};
 
 MongoClient.connect(url, function(err, database) {
     console.log("Connected successfully to server");
 
     findDocuments(database, function () {
         database.close();
-    })
+    });
 
-})
+});
