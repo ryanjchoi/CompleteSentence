605895e614ac879e00c0dac8a79ff056d14eee93
Ryan Choi <ryan.choi@cbc.ca>
2018-04-27 21:26:24 -0400

    format code

--- hapiq.js
+++ hapiq.js
@@ -15,86 +15,89 @@ var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
 server.route([
-  // Get ToDo List
-  {
-    method: 'GET',
-    path: '/api/v1/todolist',
-    handler: function(request, reply) {
-        var result = Task.find().limit(2);
-        result.exec(function(err, tasks) {
-            reply(tasks);
-        })
-    }
-  },
-
-  {
-    method: 'POST',
-    path: '/api/v1/todolist',
-    handler: function(request, reply) {
-        var newTask = new Task({
-            'author': request.payload.author,
-            'sentence': request.payload.sentence
-        });
-        newTask.save(function(err, newTask) {
-            reply(newTask).code(201)
-        })
-    }
-  },
-
-  // Get single quote
-  {
-    method: 'GET',
-    path: '/api/v1/todolist/{author}',
-    handler: function(request, reply) {
-      var result = Task.findOne({"author":request.params.author});
-      result.exec(function(err, quote) {
-          if (quote) {
-           reply(quote);
-          } else {
-            reply().code(404);
-          }
-      })
-    }
-  },
-
-  // Update single sentence
-  {
-    method: 'PUT',
-    path: '/api/v1/todolist/{author}',
-    handler: function(request, reply) {
-        var updateData = {
-            'sentence': request.payload.sentence,
-            'author': request.params.author
+    {
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            reply('Hello world from hapi');
         }
-        Task.findOneAndUpdate(
-            {'author': request.params.author},
-            updateData,
-            {new:true},
-            function(err, doc) {
-                reply(doc);
-            }
-        )
-    }
-  },
+    },
 
-  {
-    method: 'DELETE',
-    path: '/api/v1/todolist/{author}',
-    handler: function(request, reply) {
-        Task.findOneAndRemove({author:request.params.author}, function(err, response) {
-            reply().code(204);
-        });
+    // Get ToDo List
+    {
+        method: 'GET',
+        path: '/api/v1/todolist',
+        handler: function(request, reply) {
+            var result = Task.find().limit(3);
+            result.exec(function(err, tasks) {
+                reply(tasks);
+            })
+        }
+    },
+
+    // Get single quote
+    {
+        method: 'GET',
+        path: '/api/v1/todolist/{author}',
+        handler: function(request, reply) {
+            var result = Task.findOne({"author":request.params.author});
+            result.exec(function(err, quote) {
+                if (quote) {
+                    reply(quote);
+                } else {
+                    reply().code(404);
+                }
+            })
+        }
+    },
+
+    {
+        method: 'POST',
+        path: '/api/v1/todolist',
+        handler: function(request, reply) {
+            var newTask = new Task({
+                'author': request.payload.author,
+                'sentence': request.payload.sentence
+            });
+            newTask.save(function(err, newTask) {
+                reply(newTask).code(201)
+            })
+        }
+    },
+
+    // Update single sentence
+    {
+        method: 'PUT',
+        path: '/api/v1/todolist/{author}',
+        handler: function(request, reply) {
+            var updateData = {
+                'sentence': request.payload.sentence,
+                'author': request.params.author
+            }
+            Task.findOneAndUpdate(
+                {'author': request.params.author},
+                updateData,
+                {new:true},
+                function(err, doc) {
+                    reply(doc);
+                }
+            )
+        }
+    },
+
+    {
+        method: 'DELETE',
+        path: '/api/v1/todolist/{author}',
+        handler: function(request, reply) {
+            Task.findOneAndRemove(
+            {author:request.params.author},
+            function(err, response) {
+                reply().code(204);
+            });
+        }
     }
-  },
-  {
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-      reply('Hello world from hapi');
-    }
-  }
 ]);
 
 server.start(function(err) {
-  console.log('Hapi is listening to http://localhost:3000');
+    console.log('Hapi is listening to http://localhost:3000');
 });
